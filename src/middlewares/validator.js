5451fddc0c1692921424e7b73558b04fc8063fea
Ishit <ishitsingh2001@gmail.com>
2023-03-10 14:58:29 +0530

    chore : add Error Handling capability and Joi Validation

---
+++ src/middlewares/validator.js
@@ -0,0 +1,144 @@
+const Joi = require('joi');
+const { HttpError } = require('../errors/HttpError');
+
+const contentNameSchema = Joi.object(
+  {
+    contentTypeName: Joi.string().required()
+  }
+);
+
+const entryFetchSchema = Joi.object(
+  {
+    contentTypeId: Joi.number().required()
+  }
+);
+
+
+const attributeAddSchema = Joi.object(
+  {
+    attribute: Joi.string().required(),
+    contentTypeName: Joi.string().required()
+  }
+);
+
+const entrySchema = Joi.object(
+  {
+    contentTypeId: Joi.number().required(),
+    values: Joi.object().required()
+  }
+);
+
+const entryDeleteSchema = Joi.object(
+  {
+    entryId: Joi.number().required()
+  }
+);
+
+const attributeDeleteSchema = Joi.object(
+  {
+    contentTypeId: Joi.number().required(),
+    attributeName: Joi.string().required()
+  }
+);
+
+const createContentTypeValidator = (req, res, next) => {
+  try {
+    const { error } = contentNameSchema.validate(req.body);
+    if (error) {
+      throw new HttpError(error.message, 400);
+    }
+    next();
+  }
+  catch (error) {
+    if (error instanceof Joi.ValidationError) {
+      res.status(error.statusCode).json({ message: error.message });
+    }
+  }
+};
+
+const attributeAddValidator = (req, res, next) => {
+  try {
+    const { error } = attributeAddSchema.validate(req.body);
+    if (error) {
+      throw new HttpError(error.message, 400);
+    }
+    next();
+  }
+  catch (error) {
+    if (error instanceof HttpError) {
+      res.status(error.statusCode).json({ message: error.message });
+    }
+  }
+};
+
+const entryAddValidator = (req, res, next) => {
+  try {
+    const { error } = entrySchema.validate(req.body);
+    if (error) {
+      throw new HttpError(error.message, 400);
+    }
+    next();
+  }
+  catch (error) {
+    if (error instanceof HttpError) {
+      res.status(error.statusCode).json({ message: error.message });
+    }
+  }
+};
+
+
+const entryDeleteValidator = (req, res, next) => {
+  try {
+    const { error } = entryDeleteSchema.validate(req.params);
+    if (error) {
+      throw new HttpError(error.message, 400);
+    }
+    next();
+  }
+  catch (error) {
+    if (error instanceof HttpError) {
+      res.status(error.statusCode).json({ message: error.message });
+    }
+  }
+};
+
+const attributeDeleteValidator = (req, res, next) => {
+  try {
+    const { error } = attributeDeleteSchema.validate(req.params);
+    if (error) {
+      throw new HttpError(error.message, 400);
+    }
+    next();
+  }
+  catch (error) {
+    if (error instanceof HttpError) {
+      res.status(error.statusCode).json({ message: error.message });
+    }
+  }
+};
+
+const entryFetchValidator = (req, res, next) => {
+  try {
+    const { error } = entryFetchSchema.validate(req.params);
+    if (error) {
+      throw new HttpError(error.message, 400);
+    }
+    next();
+  }
+  catch (error) {
+    if (error instanceof HttpError) {
+      res.status(error.statusCode).json({ message: error.message });
+    }
+  }
+};
+
+
+module.exports = {
+  createContentTypeValidator,
+  attributeAddValidator,
+  entryAddValidator,
+  entryDeleteValidator,
+  attributeDeleteValidator,
+  entryFetchValidator
+};
+
--- src/routes/contentTypesRoutes.js
+++ src/routes/contentTypesRoutes.js
@@ -1,17 +1,25 @@
+/* eslint-disable no-unused-vars */
 const express = require('express');
 const router = express.Router();
 
 const { getContentTypesHandler, createContentTypeHandler, addAttributeToContentTypeHandler, createEntryHandler, fetchEntriesHandler, deleteAttributeHandler, deleteEntryHandler } = require('../controllers/contentTypesController');
 
+const { createContentTypeValidator,
+  attributeAddValidator,
+  entryAddValidator,
+  entryDeleteValidator,
+  attributeDeleteValidator,
+  entryFetchValidator } = require('../middlewares/validator');
+
 router.get('/contentTypes/all', getContentTypesHandler);
-router.post('/contentTypes/create', createContentTypeHandler);
-router.patch('/attributes/create', addAttributeToContentTypeHandler);
+router.post('/contentTypes/create', createContentTypeValidator, createContentTypeHandler);
+router.patch('/attributes/create', attributeAddValidator, addAttributeToContentTypeHandler);
 
-router.post('/entry/create', createEntryHandler);
-router.get('/entries/contentType/:contentTypeId', fetchEntriesHandler);
-router.delete('/attributes/delete/:contentTypeId/:attributeName', deleteAttributeHandler);
+router.post('/entry/create', entryAddValidator, createEntryHandler);
+router.get('/entries/contentType/:contentTypeId', entryFetchValidator, fetchEntriesHandler);
+router.delete('/attributes/delete/:contentTypeId/:attributeName', attributeDeleteValidator, deleteAttributeHandler);
 
-router.delete('/entry/delete/:entryId', deleteEntryHandler);
+router.delete('/entry/delete/:entryId', entryDeleteValidator, deleteEntryHandler);
 
 // const contentTypeId = req.params.contentTypeId;
 // const attributeName = req.params.attributeName;
